8e369161d06eacf77a815354331fcf26adfdf5db
wadewilsones <wadewilsones@gmail.com>
2021-12-02 12:14:54 -0500

    'ready'

--- projectJS.js
+++ projectJS.js
@@ -63,34 +63,69 @@ function loadedPage(){
   let hardSkill1 = document.getElementById("hard-skills1");
   let hardSkill2 = document.getElementById("hard-skills2");
   let hardSkill3 = document.getElementById("hard-skills3");
+
+
     //add Event Listener for click event
     submitBtn.addEventListener ('click', buildResume);
 
     function buildResume (e){
         e.preventDefault();
-        //add HTML structure and connect it with css files
-        resumePage = ('<html><head><link rel="stylesheet" href="style.css"><link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" rel="stylesheet"><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@200;400;500;600;700&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;1,100;1,300&display=swap" rel="stylesheet">');
-        resumePage += ("<title>Resume</title><body><header><h4>Ulada Haranina</h4><h5>WEB-115 Section 0001</h5><h1>Resume</h1></header><main class = 'resume-body'>");
-        //Contact section
-        resumePage += ("<section class = 'name-holder'><h2>" + name.value+"</h2><h4>" + role.value + "</h4></section><div class = 'container-for-sections'><section class ='personal'><h3>Contacts<h3><br><ul><li>");
-        resumePage += ('<i class="fas fa-home"></i>' + city.value + ", " + state.value +", " + zip.value + "</li><li>"+'<i class="fas fa-phone"></i>' + phone.value + "</li><li>"+'<i class="far fa-envelope"></i>'+email.value+"</li><li>"+'<i class="fab fa-linkedin"></i>'+linkedin.value+"</li><li>"+'<i class="fas fa-globe"></i>'+ website.value +"</li></section>");
-        resumePage += ("<section class ='summary'><h3>Profile</h3><p>"+ summary.value +"<p></div>");
-
-        //Educational section
-        resumePage += ("<h3>Education</h3><ul><li>"+ degree1.value +"</li><li>"+ uni1.value +"</li><li>"+ gradDate.value +"</li><li>"+ degree2.value +"</li><li>"+ uni2.value +"</li><li>"+ gradDate2.value +"</li>");
-
-        //Skills section
-        resumePage += ("<h3>Skills</h2><h4>Technical</h4><ul><li>"+ hardSkill1.value +"</li><li>"+ hardSkill2.value.value +"</li><li>"+ hardSkill3.value +"</li></ul><h4>Soft</h4><ul><li>"+ softSkill1.value +"</li><li>"+ softSkill2.value.value +"</li><li>"+ softSkill3.value +"</li>");
-
-        //Experience section
-        resumePage += ("<h3>Experience</h3><h4>"+position1.value+"</h4><ul><li>" + company1.value + " | " + startJobDate1.value + " - "+ endJobDate1.value + "</li></ul><p>"+details1.value+" "+"Reference: "+ reference1.value+"</p>");
-        resumePage += ("<h4>"+position2.value+"</h4><ul><li>" + company2.value + " | " + startJobDate2.value + " - "+ endJobDate2.value + "</li></ul><p>"+details2.value+" "+"Reference: "+ reference2.value+"</p>");
-        resumePage += ("<h4>"+position3.value+"</h4><ul><li>" + company3.value + " | " + startJobDate3.value + " - "+ endJobDate3.value + "</li></ul><p>"+details3.value+" "+"Reference: "+ reference3.value+"</p></main></body></html>");
-        flyWindow = window.open('about:blank','Resume');
-        flyWindow.document.write(resumePage);
+        validateForm();
     }
 
+    //Validate input
 
+    function validateForm(){
+
+       if (name.value == "" || role.value == "" || state.value == "" || zip.value == "" || state.value.length > 2 || city.value == "" || phone.value == ""){
+        alert ("Please, fill out the required fields in the contact section!");
+      }
+
+      else if (email.value == ""){
+        alert ("Fill out the email field!");
+      }
+
+      else if (summary.value.length > 300 || summary.value.length < 100){
+        alert ("Fill out the summary field! No more than 500 symbols, and no less than 300");
+      }
+
+      else if (uni1.value == "" || degree1.value == "" ){
+        alert ("Please, fill out the required fields in the Education section!");
+      }
+
+      else if (position1.value == "" || company1.value == "" || startJobDate1.value == "" || details1.value.length < 100){
+        alert ("Please, fill out the required fields in the Experience section:!");
+      }
+
+      else if (softSkill1.value == "" || hardSkill1.value == ""){
+        alert ("Please, fill out the required fields in the Skills section!");
+      }
+
+      else {
+         //add HTML structure and connect it with css files
+         resumePage = ('<html><head><link rel="stylesheet" href="style.css"><link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" rel="stylesheet"><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@200;400;500;600;700&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;1,100;1,300&display=swap" rel="stylesheet">');
+         resumePage += ("<title>Resume</title><body><header><h4>Ulada Haranina</h4><h5>WEB-115 Section 0001</h5><h1>Resume</h1></header><main class = 'resume-body'>");
+         //Contact section
+         resumePage += ("<section class = 'name-holder'><h2>" + name.value+"</h2><h4>" + role.value); // Resume header
+         resumePage += ("</h4></section><div class = 'container-for-sections'>"); // Container for contacts and personal sections
+         resumePage += ("<section class ='personal'><h3>Contacts<h3><br><ul><li>" + '<i class="fas fa-home"></i>' + city.value + ", " + state.value +", " + zip.value + "</li><li>"+'<i class="fas fa-phone"></i>' + phone.value + "</li><li>"+'<i class="far fa-envelope"></i>'+email.value+"</li><li>"+'<i class="fab fa-linkedin"></i>'+linkedin.value+"</li><li id ='website'>"+'<i class="fas fa-globe"></i>'+ website.value +"</li></section>");
+         resumePage += ("<section class ='summaryRes'><h3>Profile</h3><p>"+ summary.value +"</p></section></div>"); // Profile section and end of container
+ 
+         //Educational section
+         resumePage += ("<div class = 'container-for-sections'><section class = 'educationAndSkills'><h3>Education</h3><ul><li><h4>"+ degree1.value +"</h4></li><li>"+ uni1.value +"</li><li>"+ gradDate.value +"</li><li><h4>"+ degree2.value +"</h4></li><li>"+ uni2.value +"</li><li>"+ gradDate2.value +"</li></ul>");
+ 
+         //Skills section
+         resumePage += ("<div class = 'skills'><h3>Skills</h2><h4>Technical</h4><ul><li>"+ hardSkill1.value +"</li><li>"+ hardSkill2.value +"</li><li>"+ hardSkill3.value +"</li></ul><h4>Soft</h4><ul><li>"+ softSkill1.value +"</li><li>"+ softSkill2.value +"</li><li>"+ softSkill3.value +"</li></div></section>");
+ 
+         //Experience section
+         resumePage += ("<section class = 'experience'><h3>Experience</h3><h4>"+position1.value+"</h4><ul><li>" + company1.value + " | " + startJobDate1.value + " - "+ endJobDate1.value + "</li></ul><p>"+details1.value+" "+"Reference: "+ reference1.value+"</p>");
+         resumePage += ("<h4>"+position2.value+"</h4><ul><li>" + company2.value + " | " + startJobDate2.value + " - "+ endJobDate2.value + "</li></ul><p>"+details2.value+" "+"Reference: "+ reference2.value+"</p>");
+         resumePage += ("<h4>"+position3.value+"</h4><ul><li>" + company3.value + " | " + startJobDate3.value + " - "+ endJobDate3.value + "</li></ul><p>"+details3.value+" "+"Reference: "+ reference3.value+"</p></section></div></main></body></html>");
+      
+         flyWindow = window.open('about:blank','Resume');
+         flyWindow.document.write(resumePage);
+      }
+    }
 
 
 
